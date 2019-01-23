b4376e7e938f9a269b298796bb27122e06fa6dea
Opalee <34772199+Opalee@users.noreply.github.com>
2019-01-23 16:02:52 +0100

    Add files via upload

--- Date.java
+++ Date.java
@@ -5,6 +5,9 @@ class Date{
 	private int mois;
 	private int jour;
 
+	//variable
+	static int test=0;
+
 	//Constructeurs
 	public Date(int j,int m,int a){
 		annee = a;
@@ -16,8 +19,8 @@ class Date{
 		mois = 01;
 		jour = 01;
 	}
-    
-    //Getters
+
+  //Getters
 	public int getAnnee(){return annee;}
 	public int getMois(){return mois;}
 	public int getJour(){return jour;}
@@ -25,7 +28,7 @@ class Date{
 	//Setters
 	public void setAnnee(int a){annee = a;}
 	public void setMois(int m){mois = m;}
-	public void setJour(int j){mois = j;}
+	public void setJour(int j){jour = j;}
 
 	public String toString(){
 		return this.jour + "/" + this.mois + "/" + this.annee;
@@ -33,5 +36,63 @@ class Date{
 
 	public void afficher(){
 		System.out.println(jour + "/" + mois + "/" + annee);
+	}
+
+	public boolean estBissextile(int annee){
+		boolean verif=false;
+		if(annee%4==0 && annee%100!=0){
+			verif =true;
+		}
+		return verif;
+	}
+
+	public int compteJours(){
+		int jour=0;
+		if(this.getMois()==2){
+			if(this.estBissextile(this.getAnnee())){
+				jour=29;
+			}
+			else{jour=28;}}
+		else{
+			if(this.getMois()>7){
+				if (this.getMois()%2==0) {
+					jour=31;
+				}
+				else{
+					jour=30;
+				}
+			}
+			else{
+				if (this.getMois()%2!=0) {
+					jour=31;
+				}
+				else{
+					jour=30;
+				}
+			}
+		}
+		return jour;
+	}
+
+	public boolean estValide(){
+		boolean verif=false;
+
+		if(this.getMois() > 0 && this.getMois() < 13){
+			test++;
+		}
+		if(this.compteJours()==this.getJour()){
+			test++
+		}
+		return verif;
+	}
+
+	//Cette methode est privee car il s'agit d'une methode de classe (justification oubliee sur le site)
+	private void corrige(){
+		if(test == 0){
+			this.setMois(1);
+		}
+		else if(test == 1){
+			this.setJour(1);
+		}
 	}
 }
--- TestDate.java
+++ TestDate.java
@@ -3,15 +3,21 @@ class TestDate{
 	public static void main(String[] args) {
 		int j,m,a;
 		String s;
-		Scanner clavier = new Scanner(System.in)
+		Scanner clavier = new Scanner(System.in);
 
 		System.out.println("Entrez un jour, un mois et une année");
 		j = clavier.nextInt();
 		m = clavier.nextInt();
 		a = clavier.nextInt();
 
-		Date date1 = new Date();
-		Date date2 = new Date(j,m,a)
+		Date date1 = new Date(),
+		date2 = new Date(j,m,a);
+
+		while(!date2.estValide()){
+			date2.corrige();
+		}
+		
+		System.out.println(date2.estBissextile(date2.getAnnee()));
 
 		System.out.println("Test des Getters: ");
 		System.out.println(date1.getJour());
@@ -19,19 +25,22 @@ class TestDate{
 		System.out.println(date1.getAnnee());
 
 		System.out.println("Test des Setters: ");
-		System.out.print("Entrez de nouveau un jour: ")
+		System.out.print("Entrez de nouveau un jour: ");
 		j = clavier.nextInt();
-		System.out.println(date2.setJour(j));
-		System.out.print("Entrez de nouveau un mois: ")
+		date2.setJour(j);
+		System.out.println(date2.getJour());
+		System.out.print("Entrez de nouveau un mois: ");
 		m = clavier.nextInt();
-		System.out.println(date2.setMois(m));
-		System.out.print("Entrez de nouveau une annee: ")
+		date2.setMois(m);
+		System.out.println(date2.getMois());
+		System.out.print("Entrez de nouveau une annee: ");
 		a = clavier.nextInt();
-		System.out.println(date2.setAnnee(a));
+		date2.setAnnee(a);
+		System.out.println(date2.getAnnee());
 
 		System.out.println("Test de la methode toString puis de l'affichage: ");
 		s = date1.toString();
 		System.out.println("La variable String s correspond a la chaine: " + s);
-		date2.affichage();
+		date2.afficher();
 	}
 }
